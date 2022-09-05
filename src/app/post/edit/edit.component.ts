5079819078cff00cea20c66c901ea832f1458766
Som-Nath1997 <sonusahubanga1357@gmail.com>
2022-09-05 18:05:18 +0530

    working with crud app with avatar

--- src/app/post/edit/edit.component.ts
+++ src/app/post/edit/edit.component.ts
@@ -9,65 +9,78 @@ import {
   FormArray,
   FormBuilder,
 } from '@angular/forms';
-
+import * as data from '../country.json'
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 @Component({
   selector: 'app-edit',
   templateUrl: './edit.component.html',
   styleUrls: ['./edit.component.css'],
 })
 export class EditComponent implements OnInit {
-  
+
   id!: number;
   post!: Post;
   form!: FormGroup;
   firstname!: string;
+  products: any = (data as any).default;
+  public urlToBlob:any
+  public imageUrl! : SafeResourceUrl;
 
   constructor(
     public postService: PostService,
     private route: ActivatedRoute,
     private fb: FormBuilder,
-    private router: Router
+    private router: Router,
+    private sanitizer: DomSanitizer
   ) {}
   message: boolean = false;
 
   ngOnInit(): void {
-    
     this.id = this.route.snapshot.params['postId'];
     this.postService.find(this.id, this.firstname).subscribe((data: Post) => {
-      this.post = data;
+      console.log( data);
       this.form.patchValue({
         firstname: data.firstname,
-        lastname: data.lastname,
-        jobtitle: data.jobtitle,
         email: data.email,
-        address: data.address,
+        dob: data.dob,
+        country: data.country,
+        imageUpload:data.imageUpload
       });
     });
 
     this.form = new FormGroup({
       firstname: new FormControl('', Validators.required),
-      lastname: new FormControl('', Validators.required),
-      jobtitle: new FormControl('', Validators.required),
+      dob: new FormControl ('',[ Validators.required, Validators.pattern(/^\d{4}\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])$/)]),
+      imageUpload: new FormControl('', Validators.required),
       email: new FormControl('', [
         Validators.required,
         Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$'),
       ]),
-      address: new FormControl('', Validators.required),
-      hobbies: new FormArray([]),
+      country: new FormControl('', Validators.required),
     });
      this.fb.array([
       this.fb.control('')
     ])
   }
-  get hobbies() {
-    return this.form.get('hobbies') as FormArray;
-  }
   get f() {
     return this.form.controls;
   }
+  upload(list:any) {
+    this.urlToBlob = window.URL.createObjectURL(list.files.item(0))
+    this.imageUrl = this.sanitizer.bypassSecurityTrustUrl(this.urlToBlob);
+    console.log(list.files.item(0).name)
+  }
   submit() {
-    console.log(this.form.value);
-    this.postService.update(this.id, this.form.value).subscribe((res) => {
+    const channels:any = [
+      {
+      firstname : this.form.controls.firstname.value,
+      country : this.form.controls.country.value,
+      email : this.form.controls.email.value,
+      dob : this.form.controls.dob.value,
+      imageUpload:this.urlToBlob
+      }
+    ]
+    this.postService.update(this.id, channels[0]).subscribe((res) => {
       console.log('Employee updated successfully!');
       this.form.reset({});
       this.form.disable();
@@ -78,10 +91,5 @@ export class EditComponent implements OnInit {
     this.message = false;
     this.router.navigateByUrl('post/index');
   }
-  getHobbies() {
-    return (this.form.get('hobbies') as FormArray);
-  }
-  addAlias() {
-    this.hobbies.push(this.fb.control(''));
-  }
+
 }
